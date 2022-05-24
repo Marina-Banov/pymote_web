d6fd75678901b95495ed034f8a365ee2031528c2
mbanov <m.banov7@gmail.com>
2022-05-24 21:19:13 +0200

    status legend

--- frontend/src/app/components/network-graph/network-graph.component.ts
+++ frontend/src/app/components/network-graph/network-graph.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+
 import { PymoteNetwork, PymoteNode } from "../../models/pymote-models";
 
 @Component({
@@ -10,7 +11,7 @@ export class NetworkGraphComponent implements OnInit {
   public margin = { top: 10, right: 10, bottom: 10, left: 10 };
   public width = 620 - this.margin.left - this.margin.right;
   public height = 620 - this.margin.top - this.margin.bottom;
-  public colors = [
+  private colors = [
     "#FF0000",
     "#00FF00",
     "#0000FF",
@@ -20,7 +21,22 @@ export class NetworkGraphComponent implements OnInit {
     "#FFFFFF",
     "#000000",
   ];
-  @Input() network?: PymoteNetwork;
+  public colorMap: any = {};
+
+  public _network?: PymoteNetwork;
+  @Input() set network(n: PymoteNetwork | undefined) {
+    if (!n) return;
+    this._network = n;
+    let i = -1;
+    this._network.currentAlgorithm?.statusKeys.forEach((key, index) => {
+      if (key == "IDLE") {
+        this.colorMap[key] = this.colors[7];
+      } else {
+        i = index % 8 == 7 ? 0 : i + 1;
+        this.colorMap[key] = this.colors[i];
+      }
+    });
+  }
   @Output() selectNode: EventEmitter<PymoteNode>;
 
   constructor() {
@@ -30,10 +46,10 @@ export class NetworkGraphComponent implements OnInit {
   ngOnInit(): void {}
 
   public getCoordinate(index: any, c: string) {
-    if (typeof index == "number" && this.network) {
+    if (typeof index == "number" && this._network) {
       return c == "x"
-        ? this.network.nodes[index]?.info?.position[0]
-        : this.height - (this.network.nodes[index]?.info?.position[1] || 0);
+        ? this._network.nodes[index]?.info?.position[0]
+        : this.height - (this._network.nodes[index]?.info?.position[1] || 0);
     }
     return 0;
   }
